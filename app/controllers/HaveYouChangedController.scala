389de51c07a96c1c2fbd72a7165c123da1006455
Paul <halpo@me.com>
2025-08-01 09:12:52 +0100

    added enum to model

--- app/controllers/HaveYouChangedController.scala
+++ app/controllers/HaveYouChangedController.scala
@@ -18,11 +18,9 @@ package controllers
 
 import actions.*
 import forms.HaveYouChangedFormProvider
-
-import javax.inject.Inject
-import models.{Mode, UserAnswers}
+import javax.inject.{Inject, Singleton}
+import models.{HaveYouChangedControllerUse, Mode, UserAnswers}
 import navigation.Navigator
-import pages.{HaveYouChangedExternalPage, HaveYouChangedInternalPage, HaveYouChangedSpacePage, QuestionPage}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
@@ -31,67 +29,60 @@ import views.html.HaveYouChangedView
 import config.AppConfig
 import scala.concurrent.{ExecutionContext, Future}
 import connectors.NGRConnector
+import models.HaveYouChangedControllerUse.{getMessageKeys, pageType}
 import models.NavBarPageContents.createDefaultNavBar
 import models.registration.CredId
+import play.api.data.Form
 import uk.gov.hmrc.http.NotFoundException
 
+@Singleton
 class HaveYouChangedController @Inject()(
-                                         override val messagesApi: MessagesApi,
-                                         sessionRepository: SessionRepository,
-                                         navigator: Navigator,
-                                         identify: IdentifierAction,
-                                         getData: DataRetrievalAction,
-                                         formProvider: HaveYouChangedFormProvider,
-                                         ngrConnector: NGRConnector,
-                                         val controllerComponents: MessagesControllerComponents,
-                                         view: HaveYouChangedView
-                                 )(implicit ec: ExecutionContext, appConfig: AppConfig) extends FrontendBaseController with I18nSupport {
+                                          override val messagesApi: MessagesApi,
+                                          sessionRepository: SessionRepository,
+                                          navigator: Navigator,
+                                          identify: IdentifierAction,
+                                          getData: DataRetrievalAction,
+                                          formProvider: HaveYouChangedFormProvider,
+                                          ngrConnector: NGRConnector,
+                                          val controllerComponents: MessagesControllerComponents,
+                                          view: HaveYouChangedView
+                                        )(implicit ec: ExecutionContext, appConfig: AppConfig) extends FrontendBaseController with I18nSupport {
 
-  val form = formProvider()
+  val form: Form[Boolean] = formProvider()
 
-  def onPageLoad(use: String, mode: Mode): Action[AnyContent] = (identify andThen getData).async {
+  def onPageLoad(use: HaveYouChangedControllerUse, mode: Mode): Action[AnyContent] = (identify andThen getData).async {
     implicit request =>
 
       val credId = request.userId
       ngrConnector.getLinkedProperty(CredId(credId)).flatMap {
-        case Some(property) => {
+        case Some(property) =>
           val preparedForm = request.userAnswers.getOrElse(UserAnswers(request.userId)).get(pageType(use)) match {
             case None => form
             case Some(value) => form.fill(value)
           }
           val (title, hint) = getMessageKeys(use)
           Future.successful(Ok(view(property.addressFull, title, hint, preparedForm, use, mode, createDefaultNavBar())))
-        }
         case None => throw new NotFoundException("Property not found")
       }
   }
 
-  def getMessageKeys(use: String): (String, String) =
-    use match {
-      case "space" => ("haveYouChangedSpace.title", "haveYouChangedSpace.hint")
-      case "internal" => ("haveYouChangedInternal.title", "haveYouChangedInternal.hint")
-      case "external" => ("haveYouChangedExternal.title", "haveYouChangedExternal.hint")
-    }
-
-  private def pageType(use: String): QuestionPage[Boolean] =
-    use match {
-      case "space" => HaveYouChangedSpacePage
-      case "internal" => HaveYouChangedInternalPage
-      case "external" => HaveYouChangedExternalPage
-    }
-
-  def onSubmit(use: String, mode: Mode): Action[AnyContent] = (identify andThen getData).async {
+  def onSubmit(use: HaveYouChangedControllerUse, mode: Mode): Action[AnyContent] = (identify andThen getData).async {
     implicit request =>
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view("address", "", "", formWithErrors, use, mode, createDefaultNavBar()))),
-
-        value =>
-          val page = pageType(use)
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.getOrElse(UserAnswers(request.userId)).set(page, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(page, mode, updatedAnswers))
-      )
+      val credId = request.userId
+      ngrConnector.getLinkedProperty(CredId(credId)).flatMap {
+        case Some(property) =>
+          form.bindFromRequest().fold(
+            formWithErrors =>
+              val (title, hint) = getMessageKeys(use)
+              Future.successful(BadRequest(view(property.addressFull, title, hint, formWithErrors, use, mode, createDefaultNavBar()))),
+            value =>
+              val page = pageType(use)
+              for {
+                updatedAnswers <- Future.fromTry(request.userAnswers.getOrElse(UserAnswers(request.userId)).set(page, value))
+                _ <- sessionRepository.set(updatedAnswers)
+              } yield Redirect(navigator.nextPage(page, mode, updatedAnswers))
+          )
+        case None => throw new NotFoundException("Property not found")
+      }
   }
 }
