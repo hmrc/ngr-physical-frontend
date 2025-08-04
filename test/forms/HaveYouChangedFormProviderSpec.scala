f71a884476ccd8d0c56d74f484aa00436c6b15fa
Paul <halpo@me.com>
2025-08-04 08:44:25 +0100

    fixed test

--- test/forms/HaveYouChangedFormProviderSpec.scala
+++ test/forms/HaveYouChangedFormProviderSpec.scala
@@ -22,7 +22,7 @@ import play.api.data.FormError
 
 class HaveYouChangedFormProviderSpec extends BooleanFieldBehaviours {
 
-  val requiredKey = "haveYouChanged.error.required"
+  val requiredKey = "haveYouChangedSpace.error.required"
   val invalidKey = "error.boolean"
 
   val form = new HaveYouChangedFormProvider()(Space)
