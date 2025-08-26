db9957a8848cd68975e39b01db4ef2625fa51a1e
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2025-08-26 15:56:16 +0100

    NGR-1147 updated external small CYA messages and updated tests

--- app/models/ExternalFeature.scala
+++ app/models/ExternalFeature.scala
@@ -123,7 +123,7 @@ object ExternalFeature extends Enumerable.Implicits {
     if (value == "none") {
       messages("externalFeature.none")
     } else {
-      messages(s"externalFeature.${feature.toString}.value", value)
+      messages(s"externalFeature.$value", value)
     }
   }
 
