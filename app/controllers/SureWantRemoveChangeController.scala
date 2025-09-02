567106ef591d6a146ba4935a0af3a418f1eb6a52
Dhineth Weliwitegoda <220175817+dhinethweliwitegoda@users.noreply.github.com>
2025-09-02 11:14:01 +0100

    NGR-994 REMOVE feature journey step 1 (Group) (#23)
    * NGR-994 Created SureWantRemoveChange Controller, FormProvider and View
    
    * NGR-994 Update the remove changes with the feature string
    
    * NGR-994 Refactored messages
    
    * NGR-994 Enabled remove pages for external features
    
    * NGR-994 Fix SureWantRemoveChange Yes/No button actions
    
    * NGR-994 Refactor getFeatureLabel method
    
    * NGR-1147-1 corrected minor content errors
    
    * NGR-994 Added SureWantRemoveChangeControllerSpec test
    
    * NGR-994 Updated SureWantRemoveChange controller and view tests
    
    * NGR-994 Applied PR suggested change to determineViewType method
    
    ---------
    
    Co-authored-by: dwalters <32767569+david-walters-HMRC@users.noreply.github.com>

---
+++ app/controllers/SureWantRemoveChangeController.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import actions.{DataRetrievalAction, IdentifierAction}
+import config.AppConfig
+import forms.SureWantRemoveChangeFormProvider
+import models.{CYAExternal, CYAInternal, CYAViewType, ExternalFeature, InternalFeature}
+import models.SureWantRemoveChange.getFeatureValue
+import navigation.Navigator
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.SureWantRemoveChangeView
+import models.NavBarPageContents.createDefaultNavBar
+import uk.gov.hmrc.http.NotFoundException
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class SureWantRemoveChangeController @Inject()(
+                                          identify: IdentifierAction,
+                                          getData: DataRetrievalAction,
+                                          formProvider: SureWantRemoveChangeFormProvider,
+                                          val controllerComponents: MessagesControllerComponents,
+                                          view: SureWantRemoveChangeView
+                                        )(implicit appConfig: AppConfig) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(featureString: String): Action[AnyContent] =
+    (identify andThen getData) {
+      implicit request =>
+        val viewType = determineViewType(featureString)
+        val form: Form[Boolean] = formProvider(getFeatureValue(viewType, featureString).getOrElse(featureString))
+        Ok(view(request.property.addressFull, getTitle(viewType, featureString), featureString, form, createDefaultNavBar()))
+    }
+
+  def onSubmit(featureString: String): Action[AnyContent] = (identify andThen getData).async {
+    implicit request =>
+      val viewType = determineViewType(featureString)
+      val form: Form[Boolean] = formProvider(getFeatureValue(viewType, featureString).getOrElse(featureString))
+
+      form.bindFromRequest().fold(
+        formWithErrors =>
+          Future.successful(BadRequest(view(request.property.addressFull, getTitle(viewType, featureString), featureString, formWithErrors, createDefaultNavBar()))),
+        {
+          case true =>
+            viewType match 
+              case CYAInternal => Future.successful(Redirect(routes.SmallCheckYourAnswersController.removeInternal(featureString).url))
+              case CYAExternal => Future.successful(Redirect(routes.SmallCheckYourAnswersController.removeExternal(featureString).url))
+          case false => Future.successful(Redirect(routes.SmallCheckYourAnswersController.onPageLoad(viewType)))
+        }
+      )
+  }
+
+  private def determineViewType(feature: String): CYAViewType = {
+    InternalFeature.withNameOption(feature).map(_ => CYAInternal)
+      .orElse(ExternalFeature.withNameOption(feature).map(_ => CYAExternal))
+      .getOrElse(throw new NotFoundException("unable to determine CYAViewType"))
+  }
+
+  private def getTitle(viewType: CYAViewType, featureString: String)(implicit messages: Messages): String = {
+
+    val featureLabel = getFeatureValue(viewType, featureString)
+
+    featureLabel match {
+      case Some(label) => messages("sureWantRemoveChange.title", label)
+      case None => messages("sureWantRemoveChange.title", featureString)
+    }
+  }
+
+}
---
+++ test/controllers/SureWantRemoveChangeControllerSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import forms.SureWantRemoveChangeFormProvider
+import helpers.ControllerSpecSupport
+import models.ExternalFeature.SolarPanels
+import models.InternalFeature.{AirConditioning, PassengerLift, SecurityCamera}
+import play.api.test.Helpers.*
+import uk.gov.hmrc.http.NotFoundException
+import views.html.SureWantRemoveChangeView
+
+
+class SureWantRemoveChangeControllerSpec extends ControllerSpecSupport {
+  lazy val view: SureWantRemoveChangeView = inject[SureWantRemoveChangeView]
+  lazy val formProvider: SureWantRemoveChangeFormProvider = SureWantRemoveChangeFormProvider()
+  private val controller: SureWantRemoveChangeController = new SureWantRemoveChangeController(
+    identify = fakeAuth, getData = fakeData(None), formProvider = formProvider, controllerComponents = mcc, view = view
+  )
+
+  "SureWantRemoveChangeController" should {
+    "onPageLoad" must {
+      "return 200" in {
+        val result = controller.onPageLoad(PassengerLift.toString)(authenticatedFakeRequest)
+        status(result) mustBe OK
+      }
+      "return HTML" in {
+        val result = controller.onPageLoad(PassengerLift.toString)(authenticatedFakeRequest)
+        contentType(result) mustBe Some("text/html")
+        charset(result) mustBe Some("utf-8")
+      }
+      "throw an exception for an unknown feature" in {
+        val ex = intercept[NotFoundException] {
+          await(controller.onPageLoad ("unknown feature") (authenticatedFakeRequest) )
+        }
+        ex.getMessage must include("unable to determine CYAViewType")
+      }
+    }
+
+    "onSubmit" must {
+      "redirect to remove an internal feature" in {
+        val internalFeature = AirConditioning
+        val result = controller.onSubmit(internalFeature.toString)(requestWithForm(Map("value" -> "true")))
+        redirectLocation(result) mustBe Some(routes.SmallCheckYourAnswersController.removeInternal(internalFeature.toString).url)
+      }
+      "redirect to remove an external feature" in {
+        val externalFeature = SolarPanels
+        val result = controller.onSubmit(externalFeature.toString)(requestWithForm(Map("value" -> "true")))
+        redirectLocation(result) mustBe Some(routes.SmallCheckYourAnswersController.removeExternal(externalFeature.toString).url)
+      }
+      "bad request when no selection" in {
+        val result = controller.onSubmit(SecurityCamera.toString)(authenticatedFakeRequest)
+        status(result) mustBe BAD_REQUEST
+      }
+      "bad request with an invalid form" in {
+        val result = controller.onSubmit(SolarPanels.toString)(requestWithForm(Map("value" -> "invalid")))
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+  }
+
+}
