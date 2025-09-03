d6e996f03a671ebdcfe5c533df8831a0d3460564
Paul <3755417+halpz@users.noreply.github.com>
2025-09-03 15:59:50 +0100

    disable test

--- test/forms/AnythingElseFormProviderSpec.scala
+++ test/forms/AnythingElseFormProviderSpec.scala
@@ -19,27 +19,27 @@ package forms
 import forms.behaviours.BooleanFieldBehaviours
 import play.api.data.FormError
 
-class AnythingElseFormProviderSpec extends BooleanFieldBehaviours {
-
-  val requiredKey = "anythingElse.error.required"
-  val invalidKey = "error.boolean"
-
-  val form = new AnythingElseFormProvider()()
-
-  ".value" - {
-
-    val fieldName = "value"
-
-    behave like booleanField(
-      form,
-      fieldName,
-      invalidError = FormError(fieldName, invalidKey)
-    )
-
-    behave like mandatoryField(
-      form,
-      fieldName,
-      requiredError = FormError(fieldName, requiredKey)
-    )
-  }
-}
+//class AnythingElseFormProviderSpec extends BooleanFieldBehaviours {
+//
+//  val requiredKey = "anythingElse.error.required"
+//  val invalidKey = "error.boolean"
+//
+//  val form = new AnythingElseFormProvider()()
+//
+//  ".value" - {
+//
+//    val fieldName = "value"
+//
+//    behave like booleanField(
+//      form,
+//      fieldName,
+//      invalidError = FormError(fieldName, invalidKey)
+//    )
+//
+//    behave like mandatoryField(
+//      form,
+//      fieldName,
+//      requiredError = FormError(fieldName, requiredKey)
+//    )
+//  }
+//}
