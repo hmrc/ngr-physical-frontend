a7c0b50c7ef5447a60ac8ca175afe74974be695d
dhinethweliwitegoda <220175817+dhinethweliwitegoda@users.noreply.github.com>
2025-09-16 13:54:50 +0100

    NGR-1114 Added NotToldAnyChanges controller and view tests

---
+++ test/controllers/NotToldAnyChangesControllerSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import helpers.ControllerSpecSupport
+import models.registration.CredId
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import play.api.http.Status
+import play.api.test.Helpers.*
+import views.html.NotToldAnyChangesView
+
+import scala.concurrent.Future
+
+class NotToldAnyChangesControllerSpec
+  extends ControllerSpecSupport {
+  lazy val view: NotToldAnyChangesView = inject[NotToldAnyChangesView]
+  private val controller = new NotToldAnyChangesController(fakeAuth, fakeData(None), mcc, view)(mockConfig)
+
+  "GET /" should :
+
+    "return 200" in :
+      val result = controller.show(authenticatedFakeRequest)
+      status(result) mustBe Status.OK
+
+    "return HTML" in :
+      val result = controller.show(authenticatedFakeRequest)
+      contentType(result) mustBe Some("text/html")
+      charset(result) mustBe Some("utf-8")
+      
+}
