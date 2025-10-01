4dbd74f7de0adb52d20fe0153f08743caf3e2d4d
Paul <3755417+halpz@users.noreply.github.com>
2025-10-01 15:30:42 +0100

    Fixed onelogin integration

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,11 +8,12 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.13.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
-    "com.beachape"      %% "enumeratum-play"               % enumeratumVersion
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"                         % "12.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"                         % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                                 % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"              % "3.3.0",
+    "com.beachape"      %% "enumeratum-play"                                    % enumeratumVersion,
+    "uk.gov.hmrc"       %% "centralised-authorisation-resource-client-play-30"  % "1.10.0"
   )
 
   val test: Seq[ModuleID] = Seq(
