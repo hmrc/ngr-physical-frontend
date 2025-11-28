65aeb084d4fd098c2cff733d9be602ae84462147
ed1286 <48755019+ed1286@users.noreply.github.com>
2025-11-28 09:44:27 +0000

    NGR-3610 -version bumps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,12 +8,12 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.21.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "com.beachape"      %% "enumeratum-play"               % enumeratumVersion,
-    "uk.gov.hmrc"       %% "centralised-authorisation-resource-client-play-30"  % "1.14.0"
+    "uk.gov.hmrc"       %% "centralised-authorisation-resource-client-play-30"  % "1.15.0"
   )
 
   val test: Seq[ModuleID] = Seq(
