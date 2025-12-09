4c0a985d630324ae4c1dd70bfd578f1bef1081c5
Ashwini Rudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-12-09 10:51:52 +0000

    NGR-3468: Update libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
   private val enumeratumVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.22.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "com.beachape"      %% "enumeratum-play"               % enumeratumVersion,
     "uk.gov.hmrc"       %% "centralised-authorisation-resource-client-play-30"  % "1.15.0"
   )
@@ -20,7 +20,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "org.apache.commons"      % "commons-text"             % "1.14.0"
+    "org.apache.commons"      % "commons-text"             % "1.15.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
